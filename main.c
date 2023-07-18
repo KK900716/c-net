99d6b3dad54fe1a81b046b46528031b36492b202
HPME <443808626@qq.com>
2023-07-18 21:40:12 +0800

    feat: c-net learn!

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <winsock.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     WSADATA wasData;
     SOCKET hServSock, hClintSock;
     SOCKADDR_IN servAddr, clintAddr;
     int szClintAddr;
-    char *message = "Hello world!";
-    return 0;
-    if (WSAStartup(MAKEWORD(2, 2), &wasData) != 0) {
+    char message[] = "Hello world!";
+    if (WSAStartup(MAKEWORD(2, 2), &wasData) != 0)
         printf("error!");
-    }
-    hServSock = socket();
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServSock == INVALID_SOCKET)
+        printf("error!");
+    memset(&servAddr, 0, sizeof servAddr);
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(atoi("8080"));
+    if (bind(hServSock, (SOCKADDR*) &servAddr, sizeof servAddr) == SOCKET_ERROR)
+        printf("bind error!");
+    if (listen(hServSock, 5) == SOCKET_ERROR)
+        printf("listen error!");
+    szClintAddr = sizeof clintAddr;
+    hClintSock = accept(hServSock, (SOCKADDR*)&clintAddr, &szClintAddr);
+    if (hClintSock == INVALID_SOCKET)
+        printf("accept error!");
+    send(hClintSock, message, sizeof message, 0);
+    closesocket(hClintSock);
+    closesocket(hServSock);
+    WSACleanup();
+    return 0;
 }
